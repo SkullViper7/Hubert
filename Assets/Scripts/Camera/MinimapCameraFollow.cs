47dee5b8fcaa66a92975725424a459965043d062
Leo <leolecointre50@gmail.com>
2025-06-12 13:12:55 +0200

    Ajout sauvegarde checkpoint mais problème statue

--- Assets/Scripts/Camera/MinimapCameraFollow.cs
+++ Assets/Scripts/Camera/MinimapCameraFollow.cs
@@ -2,16 +2,8 @@ using UnityEngine;
 
 public class MinimapCameraFollow : MonoBehaviour
 {
-    [SerializeField]
-    private float _size;
-
-    [SerializeField]
-    private Transform _player;
-
     private void LateUpdate()
     {
-        transform.position = new Vector3(_player.position.x, 10, _player.position.z);
-
         Vector3 forward = Camera.main.transform.forward; 
         forward.y = 0;
         forward.Normalize();
