cce799684ec920cb9e739f3f6f2d0dbef6801963
Viper <41963246+SkullViper7@users.noreply.github.com>
2025-06-27 12:04:36 +0200

    canvas fr

--- Assets/Scripts/Menu/ProgressionReseter.cs
+++ Assets/Scripts/Menu/ProgressionReseter.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class ProgressionReseter : MonoBehaviour
 {
+    [SerializeField] GameObject _text;
+
     public void ResetProgression()
     {
         PlayerPrefs.SetInt("LastCheckpoint", 1);
         PlayerPrefs.SetInt("IsFirstLaunch", 0);
+        _text.SetActive(true);
+        Invoke(nameof(HideText), 2f);
+    }
+
+    void HideText()
+    {
+        _text.SetActive(false);
     }
 }
