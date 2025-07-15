23600346f9130803639d69aaa8adde9500e009b8
Toddgg <toddgigant2@gmail.com>
2025-07-15 18:21:11 +0200

    vfx leaves and bush wall

--- Assets/Scripts/Camera/Blur.cs
+++ Assets/Scripts/Camera/Blur.cs
@@ -7,7 +7,7 @@ public class Blur : MonoBehaviour
 {
     DepthOfField _blur;
 
-    [SerializeField, Range(0.1f, 2.5f)] float _focalDistance = 2.5f;
+    [SerializeField, Range(0f, 8f)] float _focalDistance = 8f;
     [SerializeField, Range(3f, 6f)] float _apertureSize;
 
     CinemachineFreeLook _camera;
